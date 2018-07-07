924c8289c90d22671e83e88d2ccde1967eebd8a4
zhengpinwen <zhengpinwen@cmiot.chinamobile.com>
2018-07-07 21:53:58 +0800

    particle

---
+++ app/src/main/java/com/zpw/views/exercise20/AnimListener.java
@@ -0,0 +1,24 @@
+package com.zpw.views.exercise20;
+
+import android.animation.Animator;
+
+/**
+ * Created by zpw on 2018/7/7.
+ */
+
+public abstract class AnimListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
