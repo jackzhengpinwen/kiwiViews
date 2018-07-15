6173e161a5e44e0d582678c4c273d4a0a52ff0c8
zhengpinwen <zhengpinwen@cmiot.chinamobile.com>
2018-07-15 21:26:59 +0800

    Adding a background image to an Android toolbar in the right way

---
+++ app/src/main/java/com/zpw/views/exercise26/AppBarLayoutOverScrollViewActivity.java
@@ -0,0 +1,106 @@
+package com.zpw.views.exercise26;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+
+import com.zpw.views.R;
+
+public class AppBarLayoutOverScrollViewActivity extends AppCompatActivity {
+    private final String TAG = "NestedScrollViewActivity";
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appbar_layout_0verscrollview_layout);
+        setViews();
+    }
+
+    private void setViews() {
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        FragmentManager manager = getSupportFragmentManager();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        ExampleFragmentPagerAdapter adapter = new ExampleFragmentPagerAdapter(manager);
+        viewPager.setAdapter(adapter);
+        setDrawer();
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab);
+        tabLayout.setupWithViewPager(viewPager);
+        setAppBarHeight();
+    }
+
+    private void setDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_drawer_navigation);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(select);
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener select = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            //本来ならここで分岐の処理を
+            drawerLayout.closeDrawers();
+            return true;
+        }
+    };
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+//        Log.d(TAG, "dispatchTouchEvent: ");
+        super.dispatchTouchEvent(ev);
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        Log.d(TAG, "onTouchEvent: ");
+        return super.onTouchEvent(event);
+    }
+
+    private void setAppBarHeight() {
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setLayoutParams(new CoordinatorLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight() + dpToPx(48 + 56)));
+    }
+
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    private int dpToPx(int dp) {
+        float density = getResources()
+                .getDisplayMetrics()
+                .density;
+        return Math.round((float) dp * density);
+    }
+}
---
+++ app/src/main/java/com/zpw/views/exercise26/ExampleFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.zpw.views.exercise26;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by zpw on 2018/7/15.
+ */
+public class ExampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    public ExampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return ExampleFragment.newInstance(android.R.color.holo_blue_bright);
+            case 1:
+                return ExampleFragment.newInstance(android.R.color.holo_green_light);
+            case 2:
+                return ExampleFragment.newInstance(android.R.color.holo_red_dark);
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "ページ" + (position + 1);
+    }
+}
